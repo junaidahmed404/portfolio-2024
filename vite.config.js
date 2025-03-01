d6a48a8fb1e803ca88c55409cd88821e27887509
Junaid Ahmed <bhattijunaidahmed404@gmail.com>
2025-03-01 17:05:43 +0500

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,7 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
-
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // Make sure this matches your repo name
+  base: "/", // Replace "portfolio" with your repo name
 });
